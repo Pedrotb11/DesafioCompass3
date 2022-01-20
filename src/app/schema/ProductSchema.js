03b0460a7d9c1c966c5ce17a771cbf18be97adf8
ptbelo <ptbelo@hotmail.com>
2022-01-20 11:24:16 -0300

    feat: creating productSchema and updating the schemas

---
+++ src/app/schema/ProductSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const uuid = require('uuid');
+
+const ProductSchema = mongoose.Schema({
+    product_id: {
+        type: String,
+        default: uuid.v1
+    },
+    name: String,
+    category: String,
+    price: Number,
+    employee_id: {
+        type: String,
+        ref: 'Employee'
+    },
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
