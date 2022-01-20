a504910b85978169db3bbf7069811c0cf9c7aefc
Aldemir <aldemirromario@gmail.com>
2022-01-20 20:06:38 -0300

    create validations

---
+++ src/app/validation/createEmployee.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+
+module.exports = async (req, res , next) => {
+    try {
+        const schema = Joi.object({
+            name: Joi.string().required(),
+            cpf: Joi.number().required(),
+            office: Joi.string().required(),
+            birthday:Joi.date().required().less('now')
+        });
+
+        const { error } = await schema.validate(req.body, {abortEarl: true });
+
+        if (error) throw error
+        return next ();
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+};
