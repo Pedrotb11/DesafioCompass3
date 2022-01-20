08a07f102465675b41be03a46cb0c249c35e397b
unknown <tassiolimar01@gmail.com>
2022-01-20 00:23:11 -0300

    feat database

--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -1,1 +1,13 @@
-//comment
+const DB_USER = 'lojadonamaria'
+const DB_PASSWORD = encodeURIComponent('iPHws6kDHEuXvXYq')
+
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apilojadonamaria.4z1dr.mongodb.net/lojadonamaria?retryWrites=true&w=majority`)
+.then(()=> { 
+    console.log('Database MongoDB CONNECTED!')
+    app.listen(3333)
+})
+.catch((err) => console.log(err))
+
+module.exports = mongoose;
