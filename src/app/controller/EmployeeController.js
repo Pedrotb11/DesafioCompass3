76227dc4a94140f75f4e4cdbb4bea54989550451
ptbelo <ptbelo@hotmail.com>
2022-01-21 12:52:54 -0300

    feat: creating half route of employee-creation

---
+++ src/app/controller/EmployeeController.js
@@ -0,0 +1,23 @@
+const EmployeeService = require('../service/EmployeeService');
+
+class EmployeeController {
+    async create(req, res) {
+        try {
+            const data = await EmployeeService.create(req.body);
+            return res.status(201).json({
+                'employee_id': data.employee_id,
+                'name': data.name,
+                'cpf': data.cpf,
+                'office': data.office,
+                'birthday': data.birthday,
+                'situation': data.situation,
+                'createdAt': data.createdAt,
+                'updatedAt': data.updatedAt
+            });
+        } catch (error) {
+            return res.status(500).json({ error })
+        }
+    }
+}
+
+module.exports = new EmployeeController();
